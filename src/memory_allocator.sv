39c04c7527f0adbc667886adb6fe3f60bbbcd144
noic <crouzet.noic10@gmail.com>
2023-03-14 15:18:55 +0100

    Ca marche toujours pas, mais c'est mieux enfin je crois

--- src/memory_allocator.sv
+++ src/memory_allocator.sv
@@ -1,27 +1,37 @@
 `timescale 1ns / 1ps
-
+/* verilator lint_off UNOPTFLAT */
 /* verilator lint_off UNUSEDSIGNAL */
 // allocations and de-allocations are performed in order and
 // in a circular manner (oldest allocations are last proposed)
-module memory_allocator (clk, 
+module memory_allocator (clk,
     alloc, de_alloc, reset, de_alloc_vector,
     last_alloc_addr, reset_addr, alloc_addr,
-    vector_size_is_one, vector_size_is_two,
-    vector_previous, vector_start, vector_snd, vector_thd, vector_end, vector_next);
+    vec_intf);
     parameter ADDR = 4;
     parameter DEPTH = 16;
     parameter DIRECTION = 1;
+    parameter INITIAL_ADDR = 0;
+
+    typedef struct packed{
+        logic size_is_one, size_is_two;
+        logic [ADDR-1:0] previous, head, second, third, tail, next;
+    } vector_t;
     
-    input logic clk, alloc, de_alloc, reset, de_alloc_vector, vector_size_is_one, vector_size_is_two;
+    input logic clk, alloc, de_alloc, reset, de_alloc_vector;
     input logic [ADDR-1:0] last_alloc_addr, reset_addr;
-    input logic [ADDR-1:0] vector_previous, vector_start, vector_snd, vector_thd, vector_end, vector_next;
+    input vector_t vec_intf;
     output logic [ADDR-1:0] alloc_addr;
-   
+
     logic [ADDR-1:0] next_alloc_addr, next_alloc_addr_ff;
     logic [ADDR-1:0] /*alloc_addr, */ alloc_addr_ff;
     logic [ADDR-1:0] link_next_addr, link_next_data;
     logic read_next_alloc_addr, de_alloc_vector_end;
-    
+
+    initial begin
+        next_alloc_addr_ff = ADDR'(INITIAL_ADDR + DIRECTION);
+        alloc_addr_ff = ADDR'(INITIAL_ADDR);
+    end
+
     logic [ADDR-1:0] free_vector_port_a, free_vector_port_b;
     struct {
         logic read, write;
@@ -38,7 +48,7 @@ module memory_allocator (clk,
           .web(port_b.write)
           );
     
-    assign alloc_addr = de_alloc_vector ? vector_start : alloc_addr_ff;
+    assign alloc_addr = de_alloc_vector ? vec_intf.head : alloc_addr_ff;
 
     assign next_alloc_addr = read_next_alloc_addr ? port_a.out : // read from port_a
                              next_alloc_addr_ff;                    // default from the ff
@@ -46,11 +56,11 @@ module memory_allocator (clk,
 /** logic of the BRAM control
     always_comb begin
         if(de_alloc_vector) begin
-            port_a.addr = vector_previous;
-            port_a.in    = vector_next;
+            port_a.addr = vec_intf.previous;
+            port_a.in    = vec_intf.next;
             port_a.write = 1'b1;
             port_b.addr = last_alloc_addr;
-            port_b.in    = vector_start;
+            port_b.in    = vec_intf.head;
             port_b.write = 1'b1;
         end else if(de_alloc_vector_end) begin
             port_b.addr = link_next_addr;
@@ -67,26 +77,27 @@ module memory_allocator (clk,
     end
     */
 
-    assign port_a.addr  = reset ? reset_addr : vector_previous;
-    assign port_a.in    = vector_next;
+    assign port_a.addr  = reset ? reset_addr :
+                            de_alloc_vector ? vec_intf.previous : next_alloc_addr;
+    assign port_a.in    = vec_intf.next;
     assign port_a.read  = (reset  || (alloc && !de_alloc_vector));
     assign port_a.write = de_alloc_vector;
 
     assign port_b.addr  = de_alloc_vector_end ? link_next_addr : last_alloc_addr;
-    assign port_b.in    = de_alloc_vector_end ? link_next_data : vector_start;
+    assign port_b.in    = de_alloc_vector_end ? link_next_data : vec_intf.head;
     assign port_b.read  = 1'b0;
     assign port_b.write = de_alloc_vector_end || de_alloc_vector;
        
     always_ff @(posedge clk) begin
         if(de_alloc_vector) begin
             //link_prev_addr <= last_alloc_addr;
-            //link_next_data <= vector_start;
-            link_next_addr <= vector_end;
+            //link_next_data <= vec_intf.head;
+            link_next_addr <= vec_intf.tail;
             link_next_data <= alloc_addr_ff;
             if(alloc)
-                alloc_addr_ff <= vector_size_is_one ? alloc_addr_ff : vector_snd; //vector_start is consumed
+                alloc_addr_ff <= vec_intf.size_is_one ? alloc_addr_ff : vec_intf.second; //head is consumed
             else
-                alloc_addr_ff <= vector_start; //
+                alloc_addr_ff <= vec_intf.head; //
         end else if(reset)
             alloc_addr_ff <= reset_addr; //reset to given addr
         else begin
@@ -99,10 +110,10 @@ module memory_allocator (clk,
     always_ff @(posedge clk) begin
         if(de_alloc_vector) begin
             if(alloc)
-                next_alloc_addr_ff <= vector_size_is_one ? next_alloc_addr :
-                                      (vector_size_is_two ? alloc_addr_ff : vector_thd); //vector_start and snd are consumed
+                next_alloc_addr_ff <= vec_intf.size_is_one ? next_alloc_addr :
+                                      (vec_intf.size_is_two ? alloc_addr_ff : vec_intf.third); //head and snd are consumed
             else
-                next_alloc_addr_ff <= vector_size_is_one ? alloc_addr_ff : vector_snd; //
+                next_alloc_addr_ff <= vec_intf.size_is_one ? alloc_addr_ff : vec_intf.second; //
         end else if(de_alloc) next_alloc_addr_ff <= alloc_addr_ff; //easy enough
         else if(read_next_alloc_addr) next_alloc_addr_ff <= port_a.out;
 
--- src/ras.sv
+++ src/ras.sv
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+
 /* verilator lint_off UNUSEDSIGNAL */
 module ras (clk, pop, push, branch, close_valid, close_invalid, din, dout, pop_valid);
     parameter MAXBRANCHES = 16;
@@ -6,104 +7,109 @@ module ras (clk, pop, push, branch, close_valid, close_invalid, din, dout, pop_v
     parameter DEPTH = 16;
     parameter WIDTH = 32;
     parameter ADDR = 4;
+    parameter INITIAL_ADDR = 0;
+
     input logic clk, pop, push, branch, close_valid, close_invalid;
     input logic [WIDTH-1:0] din;
     output logic [WIDTH-1:0] dout;
     output logic pop_valid;
-    
+
     logic [ADDR-1:0] BOSP/*verilator public*/;
     logic in_branch/*verilator public*/;
     logic branch_list_empty/*verilator public*/;
-    logic has_added/*verilator public*/, has_one_suppressed, has_suppressed/*verilator public*/;
-    logic [ADDR-1:0] pop_head/*verilator public*/, a_end/*verilator public*/,
-                     s_head/*verilator public*/, s_queue/*verilator public*/, s_tail/*verilator public*/,
-                     ef_start/*verilator public*/, push_head/*verilator public*/;
-    logic [ADDR-1:0] ef_start_archived, a_end_archived,
-                     s_head_archived, s_queue_archived, s_tail_archived,
-                     has_one_suppressed_archived, has_suppressed_archived;
-    logic [ADDR-1:0] pop_queue/*verilator public*/;
-    
-    
-            
-    initial BOSP = ADDR'(DEPTH - 1);
-    initial pop_head = BOSP;
-    initial push_head = pop_head + 1;
-    initial s_head = pop_head;
-    initial s_queue = s_head;
-    initial s_tail = s_head;
-    initial a_end = push_head;
-    initial ef_start = pop_head;
-    
-    logic empty/*verilator public*/; 
-        assign empty = (pop_head == BOSP);
-    logic full/*verilator public*/; 
-        assign full = (push_head == BOSP);
+    logic current_branch_has_added/*verilator public*/, current_branch_has_suppressed/*verilator public*/, vector_has_suppressed;
+    typedef struct packed {
+        logic size_is_one, size_is_two;
+        logic [ADDR-1:0] previous, head, second, third, tail, next;
+    } vector_t;
+    initial BOSP = ADDR'(INITIAL_ADDR-1);
+    initial last_alloc_addr_ff = BOSP;
+
+    logic empty/*verilator public*/;
+        assign empty = (last_alloc_addr == BOSP);
     assign pop_valid = (pop && !empty);
-    
-    logic allocate_mem, free_mem, fetch_mem, free_vec_mem;
-    logic [ADDR-1:0] alloc_addr, free_addr;
-    
-    /* verilator lint_off PINCONNECTEMPTY */     
-    
-    memory_allocator #(.ADDR(ADDR), .DEPTH(DEPTH), .INITIAL_FETCH(0), .DIRECTION(1))
+
+    logic allocate_mem, de_alloc_mem, reset_mem, de_alloc_vec_mem, read_last_alloc;
+    logic [ADDR-1:0] alloc_addr/*verilator public*/, reset_addr, last_alloc_addr/*verilator public*/, last_alloc_mem_out, last_alloc_addr_ff;
+
+    /* verilator lint_off PINCONNECTEMPTY */
+
+    vector_t current_branch_vector/*verilator public*/, free_vec_intf;
+
+    memory_allocator #(.ADDR(ADDR), .DEPTH(DEPTH), .DIRECTION(1), .INITIAL_ADDR(INITIAL_ADDR))
     memory(.clk(clk),
-            .alloc(allocate_mem), .free(free_mem), .fetch(fetch_mem), .free_vector(free_vec_mem),
-            .free_addr(free_addr), .alloc_addr(alloc_addr), .fetch_addr(a_end_archived),
-            .vector_size_is_one(has_one_suppressed_archived), 
-            .vector_start(s_head_archived), .vector_snd(s_queue_archived), .vector_end(s_tail_archived));
-             
+        .alloc(allocate_mem), .de_alloc(de_alloc_mem), .reset(reset_mem), .de_alloc_vector(de_alloc_vec_mem),
+        .last_alloc_addr(last_alloc_addr), .alloc_addr(alloc_addr), .reset_addr(reset_addr),
+        .vec_intf(free_vec_intf));
+
     bram #(.DEPTH(DEPTH), .WIDTH(32), .ADDR(ADDR))
     data(.clk(clk),
-         .doa(dout), .dia(), .addra(free_addr), .ena(pop), .wea(1'b0),
-         .dib(din), .dob(), .addrb(allocate_mem ? alloc_addr : free_addr), .enb(push), .web(push));
+        .doa(dout), .dia(), .addra(last_alloc_addr), .ena(pop), .wea(1'b0),
+        .dib(din), .dob(), .addrb(allocate_mem ? alloc_addr : last_alloc_addr), .enb(push), .web(push));
 
-    fifo #(.DEPTH(MAXBRANCHES), .WIDTH(ADDR * 5 + 2), .ADDR(BRANCHES_ADDR))
+    fifo #(.DEPTH(MAXBRANCHES), .WIDTH($bits(vector_t) + 1), .ADDR(BRANCHES_ADDR))
     branches(.clk(clk),
-             .din({ef_start, a_end, s_head, s_queue, s_tail,
-                    has_one_suppressed, has_suppressed}),
-             .dout({ef_start_archived, a_end_archived, s_head_archived, s_queue_archived, s_tail_archived,
-                    has_one_suppressed_archived, has_suppressed_archived}),
-             .wen((in_branch && branch_list_empty) || branch), 
-             .pop(((in_branch ^ branch) && branch_list_empty) || close_valid),
-             .empty(branch_list_empty),
-             .rst(close_invalid));
-     
+        .din({current_branch_vector, current_branch_has_suppressed}),
+        .dout({free_vec_intf, vector_has_suppressed}),
+        .wen((in_branch && branch_list_empty) || branch),
+        .pop(((in_branch ^ branch) && branch_list_empty) || close_valid),
+        .empty(branch_list_empty),
+        .rst(close_invalid));
+
      bram #(.DEPTH(DEPTH), .WIDTH(ADDR), .ADDR(ADDR), .OFS(-1), .BLANK(0))
      next_links(.clk(clk),
-          .doa(free_addr), .ena(allocate_mem),
-          .dia(free_addr), .addra(alloc_addr),
-          .wea(allocate_mem),
-          .dob(), .enb(1'b0),
-          .dib(), .addrb(), 
-          .web(1'b0)
-          );
+        .doa(), .ena(allocate_mem),
+        .dia(last_alloc_addr), .addra(alloc_addr),
+        .wea(allocate_mem),
+        .dob(last_alloc_mem_out), .enb(de_alloc_mem || reset_mem),
+        .dib(), .addrb(reset_mem ? free_vec_intf.previous : last_alloc_addr),
+        .web(1'b0)
+        );
+
+    assign last_alloc_addr = read_last_alloc ? last_alloc_mem_out : last_alloc_addr_ff;
+    /* verilator lint_on PINCONNECTEMPTY */
+    logic data_is_protected;
+    assign data_is_protected = (in_branch && !current_branch_has_added);
+    assign de_alloc_mem = pop && !data_is_protected && !push;
+    assign allocate_mem = push && !(pop || data_is_protected);
+    assign reset_mem = close_invalid;
+    assign reset_addr = free_vec_intf.previous;
+    assign de_alloc_vec_mem = close_valid && vector_has_suppressed;
     
-   /* verilator lint_on PINCONNECTEMPTY */
-   logic data_is_protected;
-   assign data_is_protected = (in_branch && !has_added);
-   assign free_mem = pop && !data_is_protected && !push;
-   assign allocate_mem = push && !(pop || data_is_protected);
-   assign fetch_mem = close_invalid;
-   assign free_vec_mem = close_valid && has_suppressed_archived;
-    
-   always_ff @(posedge clk) begin
-        if(branch) begin
-            in_branch <= 1'b1;
-            has_added <= 1'b0;
-            ef_start <= ;
-            a_end <= ;
-            s_head <= ;
-            s_queue <= ;
-            s_tail <= ;
-            has_suppressed <= 1'b0;
-            has_one_suppressed <= 1'b0;
-        end
-        else if(close_invalid)
+    always_ff @(posedge clk) begin
+        read_last_alloc <= de_alloc_mem || reset_mem;
+        if(allocate_mem)
+            last_alloc_addr_ff <= alloc_addr;
+        else if(read_last_alloc)
+            last_alloc_addr_ff <= last_alloc_mem_out;
+        if(close_invalid)
             in_branch <= 1'b0;
+        else if(branch) begin
+            in_branch <= 1'b1;
+            current_branch_has_added <= 1'b0;
+            current_branch_has_suppressed <= 1'b0;
+            current_branch_vector.size_is_one <= 1'b0;
+            current_branch_vector.size_is_two <= 1'b0;
+            current_branch_vector.previous <= last_alloc_addr;
+            current_branch_vector.next <= alloc_addr;
+        end
         else if(close_valid)
             in_branch <= !branch_list_empty;
-        if(in_branch) begin
-            if(push) 
+        if(pop) begin
+            if(data_is_protected) begin
+                current_branch_has_suppressed <= 1'b1;
+                current_branch_vector.head <= last_alloc_addr;
+                current_branch_vector.size_is_one <= current_branch_has_suppressed;
+                current_branch_vector.second <= current_branch_vector.head;
+                current_branch_vector.size_is_two <= current_branch_vector.size_is_one;
+                current_branch_vector.third <= current_branch_vector.second;
+                current_branch_vector.tail <= alloc_addr;
+            end else if(in_branch && current_branch_vector.previous == last_alloc_addr) begin
+                current_branch_has_added <= 1'b0;
+            end
+        end else
+        if(push && in_branch) begin
+            current_branch_has_added <= 1'b1;
         end
    end
     
