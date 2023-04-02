8540a26aac559c9062be5fbd7e723e50dfc5dc2f
noic <crouzet.noic10@gmail.com>
2023-04-03 01:01:33 +0200

    Nice grAphSS

--- test_vras.py
+++ test_vras.py
@@ -2,20 +2,33 @@ import vras
 import graphviz
 
 
+def _get_vec_info(vec):
+    fields = {"size": 2, "previous": 4,
+              "head": 4, "second": 4,
+              "third": 4, "tail": 4,
+              "next": 4}
+    out = {}
+    idx = sum(fields.values())
+    for k in fields.keys():
+        idx -= fields[k]
+        out[k] = (vec >> idx) & ((1 << fields[k]) - 1)
+    return out
+
+
 def get_infos():
+    vras.next_input()
     raw_infos = vras.raw_info()
-    names = ["alloc_addr", "last_alloc_addr",
-             "in_branch", "branch_list_empty",
-             "current_branch_has_added", "current_branch_has_suppressed",
-             "BOSP",
-             "vector_size_is_one", "vector_size_is_two",
-             "vector_previous", "vector_head",
-             "vector_second", "vector_third",
-             "vector_tail", "vector_next"]
+    names = ["prev_tosp", "tosp", "empty_start", "empty_next",
+             "in_branch", "on_branch", "branch_has_suppressed",
+             "branch_tosp", "branch_empty_start",
+             "branch_empty_next",
+             "branch_initial_tosp", "branch_initial_empty_start",
+             "bosp"]
     return dict(zip(names, raw_infos))
 
 
 def print_graph():
+    vras.next_input()
     prev = vras.free_slots()
     next = vras.next_links()
     data = vras.data()
@@ -25,8 +38,8 @@ def print_graph():
     indexes_avail = list(range(len(data)))
     with dot.subgraph() as s: # current elems
         #s.attr(rank='same')
-        i = infos["last_alloc_addr"]
-        while i != infos["BOSP"]:
+        i = infos["tosp"]
+        while i != infos["bosp"]:
             if not i in indexes_avail:
                 break
             del indexes_avail[indexes_avail.index(i)] # remove from index_avail
@@ -34,11 +47,11 @@ def print_graph():
             s.edge(str(i), str(next[i]), color='blue')
             s.edge(str(i), str(prev[i]), color='red')
             i = next[i]
-    if infos["in_branch"] and infos["current_branch_has_suppressed"]:
+    if infos["in_branch"] and infos["branch_has_suppressed"]:
         with dot.subgraph() as s: # deleted
             #s.attr(rank='same')
-            i = infos["vector_head"]
-            while i != infos["vector_next"]:
+            i = infos["branch_empty_start"]
+            while i != infos["branch_initial_empty_start"]:
                 if not i in indexes_avail:
                     break
                 del indexes_avail[indexes_avail.index(i)] # remove from index_avail
@@ -48,8 +61,8 @@ def print_graph():
                 i = prev[i]
     with dot.subgraph() as s: # free
         #s.attr(rank='same')
-        i = infos["alloc_addr"]
-        while i != infos["BOSP"]:
+        i = infos["empty_start"]
+        while i != infos["bosp"]:
             if not i in indexes_avail:
                 break
             del indexes_avail[indexes_avail.index(i)] # remove from index_avail
@@ -60,7 +73,6 @@ def print_graph():
         s.edge(str(i), str(prev[i]), color='red')
         if i in indexes_avail:
             del indexes_avail[indexes_avail.index(i)] # remove from index_avail
-
     with dot.subgraph() as s: # lost_idx
         #s.attr(rank='same')
         for lost_i in indexes_avail:
@@ -68,6 +80,19 @@ def print_graph():
             s.edge(str(lost_i), str(prev[lost_i]), color='red')
             s.edge(str(lost_i), str(next[lost_i]), color='blue')
     dot.view()
+
+def popnpush(v, verbose=False):
+    empty = vras.empty()
+    vras.next_input(pop=1, push=1, data_in=v)
+    if verbose:
+        get_infos()
+    out = vras.process()
+    if verbose:
+        get_infos()
+    print_graph()
+    if not empty:
+        return out
+    return None
 
 
 def push(v, verbose=False):
@@ -104,14 +129,17 @@ def branch(verbose=False):
     print_graph()
 
 
-def close_valid(verbose=False):
-    vras.next_input(close_valid=1)
+def close_valid(pop=False, push=None, verbose=False):
+    empty = vras.empty()
+    vras.next_input(close_valid=1, pop=pop, push=(push is not None), data_in=(push if push is not None else 0))
     if verbose:
         get_infos()
-    vras.process()
+    out = vras.process()
     if verbose:
         get_infos()
     print_graph()
+    if pop and not empty:
+        return out
 
 
 def close_invalid(verbose=False):
@@ -140,50 +168,16 @@ def init():
     vras.process()
     print_graph()
 
-"""
-from test_vras import *
-init()
-push(1)
-push(2)
-push(3)
-push(4)
-branch()
-pop()
-push(5)
-print(get_infos())
-vras.next_input(close_valid=1, push=1, data_in=6)
-print(get_infos())
-vras.process()
-print(get_infos())
-vras.next_input()
-print(get_infos())
-vras.process()
-print(get_infos())
 
-print_graph()
-print(get_infos())
-
-close_valid()
-print(get_infos())
-nop()
-print(get_infos())
-
-from test_vras import *
-init()
-push(1)
-push(2)
-push(3)
-push(4)
-branch()
-pop()
-push(5)
-vras.next_input(close_valid=1, push=1, data_in=6)
-vras.process()
-
-get_infos()
-print_graph()
-pop()
-get_infos()
-
-
-"""
+def step_exec_instrs(instrs):
+    init()
+    for i in instrs:
+        print(i)
+        empty = vras.empty()
+        vras.next_input(**i)
+        out = vras.process()
+        if 'pop' in i.keys() and not empty:
+            print("output :", out)
+        print(get_infos())
+        print_graph()
+        input()
