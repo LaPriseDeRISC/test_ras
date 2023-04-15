b0cd751c8f9cfb3b291d55c040b8608dfd9edb90
noic <crouzet.noic10@gmail.com>
2023-04-15 22:26:36 +0200

    New Fifo with passthru

--- src/fifo.sv
+++ src/fifo.sv
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
 
-module fifo (clk, rst, wen, pop, empty, din, dout);
+module fifo (clk, rst, push, pop, empty, din, dout);
     parameter DEPTH = 1024;
     parameter WIDTH = 36;
     parameter ADDR = 10;
-    input clk, rst, wen, pop;
+    input clk, rst, push, pop;
     output logic empty;
     input [WIDTH-1:0] din;
     output logic [WIDTH-1:0] dout;
@@ -18,25 +18,17 @@ module fifo (clk, rst, wen, pop, empty, din, dout);
         begin
             raddr <= '0;
             waddr <= '0;
-            empty <= '0;
-        end else begin 
-            if(pop && wen && waddr == raddr) dout <= din; //write_first
-            else dout <= ram[raddr];
-            if (pop) begin
-                 raddr <= raddr + 1'b1;
-            end 
-            if (wen) begin
-                 ram[waddr] <= din;
-                 waddr <= waddr + 1'b1;
+            empty <= 1'b1;
+        end else begin
+            if (push) begin
+                waddr <= waddr + 1'b1;
+                ram[waddr] <= din;
             end
-            if(pop ^ wen) begin
-                if(pop) 
-                    empty <= ((raddr + 1'b1 == waddr) ? 1'b1 : 1'b0);
-                else // wen
-                    empty <= 1'b0;
-            end             
+            if (pop) raddr <= raddr + 1'b1;
+            if (pop ^ push) empty <= (pop && (raddr + 1'b1 == waddr));
         end
     end
+    assign dout = empty ? din : ram[raddr];
     
 endmodule
 
