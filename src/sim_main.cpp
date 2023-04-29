f39791c6435390b60e35f63807680759d3632160
noic <crouzet.noic10@gmail.com>
2023-04-29 23:21:55 +0200

    Updated SIM and vivado project

--- src/sim_main.cpp
+++ src/sim_main.cpp
@@ -32,12 +32,11 @@ void close_module() {
     delete contextp;
 }
 
-void next_input(bool pop, bool push, bool branch, bool close_valid, bool close_invalid, uint32_t din) {
+void next_input(bool pop, bool push, std::array<bool, 2> commit, std::array<bool, 2> flush, uint32_t din) {
     top->pop = pop;
     top->push = push;
-    top->branch = branch;
-    top->close_valid = close_valid;
-    top->close_invalid = close_invalid;
+    top->commit = 0b1 * commit[0] + 0b10 * commit[1];
+    top->flush = 0b1 * flush[0] + 0b10 * flush[1];
     top->din = din;
     top->eval();
 }
@@ -48,37 +47,6 @@ uint32_t process(){
     top->clk = 1;
     top->eval();
     return top->dout;
-}
-
-TO_ARRAY_CONTAINER(top->ras->used_data->ram.m_storage) get_next_links() {
-    auto a = reinterpret_cast<TO_ARRAY_CONTAINER(
-            top->ras->used_data->ram.m_storage) &>(top->ras->used_data->ram.m_storage);
-    static_assert(sizeof(a) == sizeof(top->ras->used_data->ram.m_storage), "TO_ARRAY_CONTAINER MACRO FAILED");
-    return a;
-}
-
-TO_ARRAY_CONTAINER(top->ras->free_data->ram.m_storage) get_free_slots() {
-    auto a = reinterpret_cast<TO_ARRAY_CONTAINER(
-            top->ras->free_data->ram.m_storage) &>(top->ras->free_data->ram.m_storage);
-    static_assert(sizeof(a) == sizeof(top->ras->free_data->ram.m_storage), "TO_ARRAY_CONTAINER MACRO FAILED");
-    return a;
-}
-
-TO_ARRAY_CONTAINER(top->ras->data->ram.m_storage) get_data() {
-    auto a = reinterpret_cast<TO_ARRAY_CONTAINER(top->ras->data->ram.m_storage) &>(top->ras->data->ram.m_storage);
-    static_assert(sizeof(a) == sizeof(top->ras->data->ram.m_storage), "TO_ARRAY_CONTAINER MACRO FAILED");
-    return a;
-}
-
-std::array<uint32_t,13> raw_infos() {
-    return {top->ras->prev_tosp, top->ras->tosp,
-            top->ras->empty_start, top->ras->empty_next,
-            top->ras->in_branch, top->ras->on_branch, top->ras->branch_has_suppressed,
-            top->ras->branch_tosp,
-            top->ras->branch_empty_start, top->ras->branch_empty_next,
-            top->ras->branch_initial_tosp, top->ras->branch_initial_empty_start,
-            top->ras->bosp
-    };
 }
 
 PYBIND11_MODULE(vras, m
@@ -92,16 +60,11 @@ m.def("process", &process, "Step in clock");
 m.def("next_input", &next_input, "Step in clock",
 py::arg("pop") = false,
 py::arg("push") = false,
-py::arg("branch") = false,
-py::arg("close_valid") = false,
-py::arg("close_invalid") = false,
+py::arg("commit") = std::array<bool, 2>{false, false},
+py::arg("flush") = std::array<bool, 2>{false, false},
 py::arg("data_in") = 0);
 m.def("close", &close_module, "Close the module");
 m.def("reset", &reset, "resets the module");
-m.def("next_links", &get_next_links, "next ptrs");
-m.def("free_slots", &get_free_slots, "prev ptrs");
 m.def("empty", [](){return top->empty;});
-m.def("raw_info", &raw_infos);
-m.def("data", &get_data, "data");
 }
 
