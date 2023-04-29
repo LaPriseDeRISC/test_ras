c87f67f802f90b9e64eb825f5bc8b6d17aa22ad8
noic <crouzet.noic10@gmail.com>
2023-04-29 23:20:33 +0200

    New collision-correction BRAM

--- src/ras_bram.sv
+++ src/ras_bram.sv
@@ -3,26 +3,25 @@ module ras_bram (clk,rea,reb,wea,web,raddra,raddrb,waddra,waddrb,wia,wib,doa,dob
     parameter DEPTH = 1024;
     parameter WIDTH = 36;
     localparam ADDR = $clog2(DEPTH);
-    parameter OFS = 0;
-    parameter INCR = 0;
+    parameter RESOLVE_COLLIDE = 0;
     input clk,rea,reb,wea,web;
     input [ADDR-1:0] raddra, raddrb;
     input [ADDR-1:0] waddra, waddrb;
     input [WIDTH-1:0] wia, wib;
     output logic [WIDTH-1:0] doa,dob;
     reg [WIDTH-1:0] ram [DEPTH-1:0]/*verilator public*/;
-    generate
-        for(genvar i = 0; i< DEPTH; i++) begin
-            initial ram[i] = WIDTH'(OFS + (i * INCR));
-        end
-    endgenerate 
-    
-    logic [ADDR-1:0] addra, addrb;
-    logic ena, enb;
-    assign addra = wea ? waddra : raddra;
-    assign addrb = web ? waddrb : raddrb;
-    assign ena = rea || wea;
-    assign enb = reb || web;
+
+    wire collision = RESOLVE_COLLIDE && (addra == addrb) && (rea || wea) && (reb || web);
+    wire forward_a = collision && web;
+    wire forward_b = collision && wea;
+    wire [ADDR-1:0] addra = wea ? waddra : raddra;
+    wire [ADDR-1:0] addrb = web ? waddrb : raddrb;
+    wire ena = (rea || wea) && !forward_a;
+    wire enb = (reb || web) && !forward_b;
+    logic [WIDTH-1:0] ram_out_a, ram_out_b;
+
+    assign doa = forward_a ? ram_out_b : ram_out_a;
+    assign dob = forward_b ? ram_out_a : ram_out_b;
 
     always @(posedge clk)
     begin
@@ -30,8 +29,8 @@ module ras_bram (clk,rea,reb,wea,web,raddra,raddrb,waddra,waddrb,wia,wib,doa,dob
         begin
             if (wea) begin
                 ram[addra] <= wia;
-                doa <= wia;
-            end else doa <= ram[addra];
+                ram_out_a <= wia;
+            end else ram_out_a <= ram[addra];
         end
     end
 
@@ -41,8 +40,8 @@ module ras_bram (clk,rea,reb,wea,web,raddra,raddrb,waddra,waddrb,wia,wib,doa,dob
         begin
             if (web) begin
                 ram[addrb] <= wib;
-                dob <= wib;
-            end else dob <= ram[addrb];
+                ram_out_b <= wib;
+            end else ram_out_b <= ram[addrb];
         end
     end
 endmodule 
